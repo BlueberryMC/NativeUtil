90372a33d221a78d80ae9b91445caca3bcd0471a
acrylic-style <me@acrylicstyle.xyz>
2022-04-08 11:52:52 +0900

    Add @Documented annotation to @NativeType

--- src/main/java/net/blueberrymc/nativeutil/NativeType.java
+++ src/main/java/net/blueberrymc/nativeutil/NativeType.java
@@ -2,6 +2,7 @@ package net.blueberrymc.nativeutil;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,9 +11,14 @@ import java.lang.annotation.Target;
 /**
  * This annotation is used to mark a native type. This is not used anywhere in the NativeUtil.
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.PARAMETER })
 public @interface NativeType {
+    /**
+     * The type of the native type.
+     * @return type name
+     */
     @NotNull
     String value();
 }
